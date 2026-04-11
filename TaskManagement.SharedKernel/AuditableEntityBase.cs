c83d8e5beb5ba39255e744b1804a63dcfb8df1db
lesan1995 <lesan1995@gmail.com>
2026-04-11 14:55:57 +0700

    Add audit entity base

---
+++ TaskManagement.SharedKernel/AuditableEntityBase.cs
@@ -0,0 +1,29 @@
+﻿namespace TaskManagement.SharedKernel
+{
+    public class AuditableEntityBase<T, TId> : EntityBase<T, TId> where T : EntityBase<T, TId>
+    {
+        public string CreatedBy { get; private set; } = string.Empty;
+        public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+        public string? LastModifiedBy { get; private set; }
+        public DateTime? LastModifiedAt { get; private set; }
+        public bool IsDeleted { get; private set; } = false;
+        public string? DeletedBy { get; private set; }
+        public DateTime? DeletedAt { get; private set; }
+        protected void SetCreated(string createdBy)
+        {
+            CreatedBy = createdBy;
+            CreatedAt = DateTime.UtcNow;
+        }
+        protected void SetModified(string modifiedBy)
+        {
+            LastModifiedBy = modifiedBy;
+            LastModifiedAt = DateTime.UtcNow;
+        }
+        protected void SoftDelete(string deletedBy)
+        {
+            IsDeleted = true;
+            DeletedBy = deletedBy;
+            DeletedAt = DateTime.UtcNow;
+        }
+    }
+}
