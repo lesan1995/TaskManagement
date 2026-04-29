7f7442b16f3c9a290e1d2d216715a9cbc5622ba3
lesan1995 <lesan1995@gmail.com>
2026-04-29 15:40:39 +0700

    Add mapper project

---
+++ TaskManagement.UseCases/Projects/DTO/Mapper.cs
@@ -0,0 +1,49 @@
+﻿using TaskManagement.SharedKernel.User;
+
+namespace TaskManagement.UseCases.Projects.DTO
+{
+    public static class Mapper
+    {
+        public static ProjectDetailDTO MapToProjectDetailDto(this Project project, Dictionary<string, UserInfo> userInfos)
+        {
+            return new ProjectDetailDTO(
+                Id: project.Id,
+                Name: project.Name,
+                Description: project.Description,
+                Deadline: project.Deadline,
+                Status: project.Status,
+                Progress: project.Progress,
+                Members: project.Members.MapToMemberDtos(userInfos),
+                Tasks: project.Tasks.MapToTaskItemDtos(userInfos),
+                Issues: project.Issues.MapToTaskItemDtos(userInfos)
+                );
+        }
+        public static List<ProjectMemberDTO> MapToMemberDtos(this IReadOnlyCollection<ProjectMember> members, Dictionary<string, UserInfo> userInfos)
+        {
+            return members.Select(m => new ProjectMemberDTO(
+                    UserInfo: userInfos.GetValueOrDefault(m.UserId.ToString()) ?? new UserInfo { UserId = m.UserId.ToString() },
+                    Role: m.Role)).ToList();
+        }
+        public static List<TaskItemDTO> MapToTaskItemDtos(this IReadOnlyCollection<TaskItem> tasks, Dictionary<string, UserInfo> userInfos)
+        {
+            return tasks.Select(task => new TaskItemDTO(
+                    Title: task.Title,
+                    Description: task.Description,
+                    IsDone: task.IsDone,
+                    Assignee: task.AssigneeId.HasValue ? userInfos.GetValueOrDefault(task.AssigneeId.Value.ToString()) : null,
+                    OverIndex: task.OverIndex,
+                    Attachments: task.Attachments.Select(attachment => attachment.FileUrl).ToList()
+                    )).ToList();
+        }
+        public static List<IssueDTO> MapToTaskItemDtos(this IReadOnlyCollection<Issue> issues, Dictionary<string, UserInfo> userInfos)
+        {
+            return issues.Select(issue => new IssueDTO(
+                    Content: issue.Content,
+                    Severity: issue.Severity,
+                    IsResolved: issue.IsResolved,
+                    ResolvedComment: issue.ResolvedComment,
+                    Attachments: issue.Attachments.Select(attachment => attachment.FileUrl).ToList()
+                    )).ToList();
+        }
+    }
+}
--- TaskManagement.UseCases/Projects/Get/GetProjectHandler.cs
+++ TaskManagement.UseCases/Projects/Get/GetProjectHandler.cs
@@ -1,7 +1,6 @@
 ﻿using System.Net.Mail;
 using TaskManagement.Core.ProjectAggregate.Specifications;
 using TaskManagement.Core.User;
-using TaskManagement.SharedKernel.User;
 using TaskManagement.UseCases.Projects.DTO;
 
 namespace TaskManagement.UseCases.Projects.Get
@@ -17,36 +16,10 @@ namespace TaskManagement.UseCases.Projects.Get
             if (project == null)
                 return Result<ProjectDetailDTO>.NotFound();
 
-            var memberInfos = await userService.GetUsersInfoAsync(
-                project.Members.Select(x => x.UserId.ToString()), 
-                ct);
+            var memberInfos = await userService.GetUsersInfoAsync(project.Members.Select(x => x.UserId.ToString()), ct);
 
-            var projectDto = new ProjectDetailDTO(
-                Id: project.Id,
-                Name: project.Name,
-                Description: project.Description,
-                Deadline: project.Deadline,
-                Status: project.Status,
-                Progress: project.Progress,
-                Members: project.Members.Select(m => new ProjectMemberDTO(
-                    UserInfo: memberInfos.GetValueOrDefault(m.UserId.ToString()) ?? new UserInfo(),
-                    Role: m.Role)).ToList(),
-                Tasks: project.Tasks.Select(task => new TaskItemDTO(
-                    Title: task.Title,
-                    Description: task.Description,
-                    IsDone: task.IsDone,
-                    Assignee: task.AssigneeId != null ?
-                        (memberInfos.TryGetValue(task.AssigneeId.Value.ToString(), out var userInfo) ? userInfo : null)
-                        : null,
-                    OverIndex: task.OverIndex,
-                    Attachments: task.Attachments.Select(attachment => attachment.FileUrl).ToList())).ToList(),
-                Issues: project.Issues.Select(issue => new IssueDTO(
-                    Content:  issue.Content,
-                    Severity:  issue.Severity,
-                    IsResolved:  issue.IsResolved,
-                    ResolvedComment: issue.ResolvedComment,
-                    Attachments: issue.Attachments.Select(attachment => attachment.FileUrl).ToList())).ToList()
-                );
+            var projectDto = project.MapToProjectDetailDto(memberInfos);
+
             return Result<ProjectDetailDTO>.Success(projectDto);
         }
     }
