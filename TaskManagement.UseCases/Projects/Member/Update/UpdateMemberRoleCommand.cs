094209020201f1255efb9962409614d0ccc5fe5b
lesan1995 <lesan1995@gmail.com>
2026-04-30 12:41:26 +0700

    Update member

---
+++ TaskManagement.UseCases/Projects/Member/Update/UpdateMemberRoleCommand.cs
@@ -0,0 +1,4 @@
+﻿namespace TaskManagement.UseCases.Projects.Member.Remove
+{
+    public record UpdateMemberRoleCommand(ProjectId ProjectId, UserId UserId, ProjectMemberRole Role) : ICommand<Result>;
+}
---
+++ TaskManagement.UseCases/Projects/Member/Update/UpdateMemberRoleHandler.cs
@@ -0,0 +1,26 @@
+﻿namespace TaskManagement.UseCases.Projects.Member.Remove
+{
+    public class UpdateMemberRoleHandler(
+        IRepository<Project> repository,
+        ICurrentUserService currentUser)
+        : ICommandHandler<UpdateMemberRoleCommand, Result>
+    {
+        public async ValueTask<Result> Handle(UpdateMemberRoleCommand command, CancellationToken ct)
+        {
+            var spec = new ProjectByIdWithMemberSpec(command.ProjectId);
+            var project = await repository.FirstOrDefaultAsync(spec, ct);
+            if (project == null)
+                return Result.NotFound();
+
+            bool hasPermission = currentUser.IsManager || project.IsProjectManager(currentUser.UserId);
+            if (!hasPermission)
+                return Result<ProjectMemberDTO>.Forbidden("You do not have permission to update role of members");
+
+            project.UpdateMemberRole(command.UserId, command.Role);
+
+            await repository.UpdateAsync(project);
+
+            return Result.Success();
+        }
+    }
+}
