c8e979eaf2db068a7bdb16f7d66c3fc8d49ebf9a
lesan1995 <lesan1995@gmail.com>
2026-05-01 22:30:49 +0700

    add file storage service

---
+++ TaskManagement.SharedKernel/File/IFileStorageService.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManagement.SharedKernel.File
+{
+    public interface IFileStorageService
+    {
+        Task<string> UploadAsync(Stream fileStream, string fileName, string contentType, CancellationToken ct = default);
+        Task<bool> DeleteAsync(string fileUrl, CancellationToken ct = default);
+        Task<bool> DeletesAsync(IEnumerable<string> fileUrls, CancellationToken ct = default);
+        Task<Stream> DownloadAsync(string fileUrl, CancellationToken ct = default);
+    }
+}
