1221812ab45eef12a8a302cbd56a21bbcd48b4e1
lesan1995 <lesan1995@gmail.com>
2026-05-01 22:31:18 +0700

    add unitofwork

---
+++ TaskManagement.SharedKernel/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskManagement.SharedKernel
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        Task BeginTransactionAsync(CancellationToken ct = default);
+        Task CommitAsync(CancellationToken ct = default);
+        Task RollBackAsync(CancellationToken ct = default);
+    }
+}
