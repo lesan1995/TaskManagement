d2690a830f5f44c64d99ea960b6e1f87a5d26498
lesan1995 <lesan1995@gmail.com>
2026-05-02 15:31:48 +0700

    delete project command

---
+++ TaskManagement.UseCases/Projects/Delete/DeleteProjectCommand.cs
@@ -0,0 +1,4 @@
+﻿namespace TaskManagement.UseCases.Projects.Delete
+{
+    public record DeleteProjectCommand(ProjectId ProjectId) : ICommand<Result>;
+}
---
+++ TaskManagement.UseCases/Projects/Delete/DeleteProjectHandler.cs
@@ -0,0 +1,20 @@
+﻿namespace TaskManagement.UseCases.Projects.Delete
+{
+    public class DeleteProjectHandler(
+        IRepository<Project> repository,
+        ICurrentUserService currentUser) : ICommandHandler<DeleteProjectCommand, Result>
+    {
+        public async ValueTask<Result> Handle(DeleteProjectCommand command, CancellationToken ct)
+        {
+            if (!currentUser.IsManager)
+                return Result.Forbidden("Only manager can be delete project");
+            var spec = new ProjectByIdBasicSpec(command.ProjectId);
+            var project = await repository.FirstOrDefaultAsync(spec, ct);
+            if (project == null)
+                return Result.NotFound();
+            project.SoftDelete(currentUser.UserId.ToString());
+            await repository.UpdateAsync(project, ct);
+            return Result.Success();
+        }
+    }
+}
