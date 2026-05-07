96e46a9e6172251be12c6bcef61a9b1a71b0ae4a
lesan1995 <lesan1995@gmail.com>
2026-05-07 15:19:28 +0700

    Add notification configuration

---
+++ TaskManagement.Infrastructure/Data/Config/NotificationConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TaskManagement.Core.NotificationAggregate;
+
+namespace TaskManagement.Infrastructure.Data.Config
+{
+    public class NotificationConfiguration : IEntityTypeConfiguration<Notification>
+    {
+        public void Configure(EntityTypeBuilder<Notification> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                .HasConversion(
+                    id => id.Value,
+                    value => NotificationId.Create(value))
+                .IsRequired()
+                .ValueGeneratedOnAdd();
+
+            builder.Property(x => x.Content)
+                .HasConversion(
+                    content => content.Value,
+                    value => NotificationContent.Create(value))
+                .HasMaxLength(NotificationContent.MaxLength)
+                .IsRequired();
+
+            builder.Property(x => x.CreatedAt)
+                .IsRequired();
+
+            builder.HasMany(x => x.Users)
+                .WithOne()
+                .HasForeignKey(x => x.NotificationId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            var userNavigation = builder.Metadata.FindNavigation(nameof(Notification.Users));
+            userNavigation?.SetPropertyAccessMode(PropertyAccessMode.Field);
+        }
+    }
+}
---
+++ TaskManagement.Infrastructure/Data/Config/NotificationUserConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TaskManagement.Core.NotificationAggregate;
+using TaskManagement.SharedKernel.User;
+
+namespace TaskManagement.Infrastructure.Data.Config
+{
+    public class NotificationUserConfiguration : IEntityTypeConfiguration<NotificationUser>
+    {
+        public void Configure(EntityTypeBuilder<NotificationUser> builder)
+        {
+            builder.Property(x => x.UserId)
+                .HasConversion(
+                    x => x.Value,
+                    value => UserId.Create(value))
+                .IsRequired();
+
+            builder.Property(x => x.IsRead)
+                .IsRequired();
+        }
+    }
+}
