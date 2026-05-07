4541eb29ff6607f3896b478a472c9d184f803640
lesan1995 <lesan1995@gmail.com>
2026-05-07 15:35:03 +0700

    Add Efrepository

---
+++ TaskManagement.Infrastructure/Data/EfRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace TaskManagement.Infrastructure.Data
+{
+    public class EfRepository<T>(AppDbContext dbContext) :
+        RepositoryBase<T>(dbContext), IReadRepository<T>, IRepository<T> where T : class, IAggregateRoot
+    {
+    }
+}
--- TaskManagement.Infrastructure/GlobalUsings.cs
+++ TaskManagement.Infrastructure/GlobalUsings.cs
@@ -1,2 +1,4 @@
 ﻿global using Microsoft.EntityFrameworkCore;
 global using System.Reflection;
+global using Ardalis.Specification.EntityFrameworkCore;
+global using TaskManagement.SharedKernel;
