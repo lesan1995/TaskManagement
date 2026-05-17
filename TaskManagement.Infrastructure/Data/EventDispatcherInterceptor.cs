5d71de10fbbd257417c7ecded571d55589c16339
lesan1995 <lesan1995@gmail.com>
2026-05-17 21:14:50 +0700

    Add event interceptor

---
+++ TaskManagement.Infrastructure/Data/EventDispatcherInterceptor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace TaskManagement.Infrastructure.Data
+{
+    public class EventDispatcherInterceptor(IDomainEventDispatcher domainEventDispatcher) : SaveChangesInterceptor
+    {
+        private readonly IDomainEventDispatcher _domainEventDispatcher = domainEventDispatcher;
+
+        public override async ValueTask<int> SavedChangesAsync(SaveChangesCompletedEventData eventData, int result, CancellationToken cancellationToken = new CancellationToken())
+        {
+            var context = eventData.Context;
+            if(context is not AppDbContext appDbContext)
+            {
+                return await base.SavedChangesAsync(eventData, result, cancellationToken);
+            }
+
+            var entitiesWithEvents = appDbContext.ChangeTracker.Entries<HasDomainEventsBase>()
+                .Select(e => e.Entity)
+                .Where(e => e.DomainEvents.Any())
+                .ToArray();
+
+            var resultSaveChange = await base.SavedChangesAsync(eventData, result, cancellationToken);
+
+            await _domainEventDispatcher.DispatchAndClearEvents(entitiesWithEvents);
+
+            return resultSaveChange;
+        }
+    }
+}
