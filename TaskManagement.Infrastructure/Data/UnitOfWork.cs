b1fe3794590ff443042f1070713b03f51e01f521
lesan1995 <lesan1995@gmail.com>
2026-05-17 22:25:36 +0700

    implement unitofwork

---
+++ TaskManagement.Infrastructure/Data/UnitOfWork.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+
+namespace TaskManagement.Infrastructure.Data
+{
+    public class UnitOfWork(AppDbContext dbContext) : IUnitOfWork
+    {
+        private IDbContextTransaction? _transaction; 
+        public async Task BeginTransactionAsync(CancellationToken ct = default)
+        {
+            if(_transaction != null){
+                await _transaction.RollbackAsync();
+            };
+            _transaction = await dbContext.Database.BeginTransactionAsync(ct);
+        }
+
+        public async Task CommitAsync(CancellationToken ct = default)
+        {
+            if(_transaction != null)
+            {
+                await _transaction.CommitAsync(ct);
+                await _transaction.DisposeAsync();
+                _transaction = null;
+            }
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (_transaction != null)
+            {
+                await _transaction.DisposeAsync();
+                _transaction = null;
+            }
+        }
+
+        public async Task RollbackAsync(CancellationToken ct = default)
+        {
+            if (_transaction != null)
+            {
+                await _transaction.RollbackAsync(ct);
+                _transaction.Dispose();
+                _transaction = null;
+            }
+        }
+    }
+}
--- TaskManagement.SharedKernel/IUnitOfWork.cs
+++ TaskManagement.SharedKernel/IUnitOfWork.cs
@@ -1,6 +1,6 @@
 ﻿namespace TaskManagement.SharedKernel
 {
-    public interface IUnitOfWork : IDisposable
+    public interface IUnitOfWork : IAsyncDisposable
     {
         Task BeginTransactionAsync(CancellationToken ct = default);
         Task CommitAsync(CancellationToken ct = default);
