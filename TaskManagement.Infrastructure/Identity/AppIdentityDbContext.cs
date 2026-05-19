8a083f49958068f0ca53aae676d4356be42773cc
lesan1995 <lesan1995@gmail.com>
2026-05-19 08:33:19 +0700

    Add indentity db context

---
+++ TaskManagement.Infrastructure/Identity/AppIdentityDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace TaskManagement.Infrastructure.Identity
+{
+    public class AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ TaskManagement.Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TaskManagement.Infrastructure.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
